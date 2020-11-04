0187e69f6ed8aa580c2e0b4d5d1a2a2a1e22dfaf
Daniele <danlup94@gmail.com>
2020-11-04 16:51:56 +0100

    Inserimento codice js

--- myscript.js
+++ myscript.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+$(".tasto").click(vediMenu);
+
 })
 
 // DICHIARAZIONE FUNZIONI
+
+// Al click sulla nav vedo i dropdown
+function vediMenu() {
+
+$(".dropmenu").addClass("active");
+
+}
